e1eb9936792fb3a681dc64b5d9df61b1ba01b13c
Naufallm <an4667112@gmail.com>
2023-10-30 10:49:53 +0700

    first commit

---
+++ src/main/java/proglan/Main.java
@@ -0,0 +1,40 @@
+package proglan;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Inisialisasi harga barang
+        int hargaApple = 10000;
+        int hargaPisang = 10000;
+        int hargaAnggur = 10000;
+
+
+        // Inisialisasi jumlah barang
+        int jumlahApple = 0;
+        int jumlahPisang = 0;
+        int jumlahAnggur = 0;
+
+
+        // Scanner untuk input dari pengguna
+        Scanner scanner = new Scanner(System.in);
+
+        // Meminta input jumlah masing-masing barang
+        System.out.print("Masukkan jumlah apple yang dibeli: ");
+        jumlahApple = scanner.nextInt();
+        System.out.print("Masukkan jumlah pisang yang dibeli: ");
+        jumlahPisang = scanner.nextInt();
+        System.out.print("Masukkan jumlah anggur yang dibeli: ");
+        jumlahAnggur = scanner.nextInt();
+
+
+
+        // Menghitung total harga
+        int totalHarga = (jumlahApple * hargaApple) + (jumlahPisang * hargaPisang) + (jumlahAnggur * hargaAnggur);
+
+        // Menampilkan total harga
+        System.out.println("Total harga: " + totalHarga + " Rupiah");
+
+        // Menutup scanner
+        scanner.close();
+    }
+}
